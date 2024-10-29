c85a545fe1ebec2c1775b05f01fffe8635d5f581
Daniel George <120543571+datcuriousguy@users.noreply.github.com>
2024-10-29 23:02:48 +0530

    Update ROOMMATE APP BETA.py
    OCTOBER 29TH 2024:
      I have added GET and POST methods for most functions for redundancy though any one of GET or  POST could be used.

--- ROOMMATE APP BETA.py
+++ ROOMMATE APP BETA.py
@@ -1,4 +1,3 @@
-
 """
 
 
@@ -20,14 +19,42 @@ CORS(app)
 
 db_connection = mysql.connector.connect(
     host="localhost",
-    user="root",
-    password="your_local_mysql_password",
+    user="your_mysql_username",
+    password="your_mysql_password",
     database="roommate_app"
 )
 
 cur = db_connection.cursor()
 
 
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     data = request.get_json()
+#     username = data.get('username')
+#     password = data.get('password')
+#
+#     print(f'username: {username}')
+#     print(f'password: {password}')
+#     print(verify_username_password(username, password))
+#
+#     return username, password
+
+
+# def verify_username_password(username, password):
+#
+#     statement = f"SELECT university_email, password FROM student_info WHERE university_email = {username};"
+#     check_db_for_credentials = cur.execute(statement)
+#
+#     print(check_db_for_credentials[0])
+#     print(check_db_for_credentials[1])
+
+"""
+OCTOBER 29TH 2024:
+  I have added GET and POST methods for most functions for redundancy though any one of GET or  POST could be used.
+  
+  """
+
+# @app.route('/get_student_ids_by_gender', methods=['GET', 'POST'])
 def get_student_ids_by_gender(gender):
     student_ids_of_gender = cur.execute(
         f"SELECT student_id, first_name, last_name FROM student_info WHERE gender = '{gender}';'")
@@ -36,22 +63,24 @@ def get_student_ids_by_gender(gender):
     student_ids_of_chosen_gender_list = []
     for student_id_of_chosen_gender in student_ids_of_chosen_gender:
         student_ids_of_chosen_gender_list.append(student_id_of_chosen_gender)
+        #print(student_id_of_chosen_gender)
     # I used this line for testing the function in isolation: print(student_ids_of_chosen_gender_list)
     return student_ids_of_chosen_gender_list
 
 
-# @app.route('/api/get_student_id_by_bedtime_preference/')
+@app.route('/get_student_id_by_bedtime_preference', methods=['GET', 'POST'])
 def get_student_id_by_bedtime_preference(gender, bedtime_preference):
+
     db_connection = mysql.connector.connect(
         host="localhost",
-        user="root",
-        password="your_local_mysql_password",
+        user="your_mysql_username",
+        password="your_mysql_password",
         database="roommate_app"
     )
 
     cur = db_connection.cursor()
     student_info = get_student_ids_by_gender(gender)
-    print(f'Students with bedtime {bedtime_preference}:\n\nStudent_id | Name\n\n')
+    print(f'Students with bedtime {bedtime_preference}:00\n\nStudent_id | Name\n\n')
     for student_info_row in student_info:
         student_info_row_id = student_info_row[0]
         student_info_row_first_name = student_info_row[1]
@@ -66,9 +95,10 @@ def get_student_id_by_bedtime_preference(gender, bedtime_preference):
         # print(student_ids_with_specified_bedtime_fetchall)
         # for student_id in student_ids_with_specified_bedtime_fetchall:
         #     print(f'{student_ids[0]} {student_ids[1]} prefers bedtime {bedtime_preference}.')
+        return student_info_row_fetchall
 
 
-# @app.route('/api/get_student_id_by_ac_fan_preference/')
+@app.route('/get_student_id_by_ac_fan_preference', methods=['GET', 'POST'])
 def get_student_id_by_ac_fan_preference(gender, ac_fan_preference):
     """
         add if statement to use conditions:
@@ -95,8 +125,8 @@ def get_student_id_by_ac_fan_preference(gender, ac_fan_preference):
 
     db_connection = mysql.connector.connect(
         host="localhost",
-        user="root",
-        password="your_local_mysql_password",
+        user="your_mysql_username",
+        password="your_mysql_password",
         database="roommate_app"
     )
 
@@ -113,8 +143,8 @@ def get_student_id_by_ac_fan_preference(gender, ac_fan_preference):
 
             db_connection = mysql.connector.connect(
                 host="localhost",
-                user="root",
-                password="your_local_mysql_password",
+                user="your_mysql_username",
+                password="your_mysql_password",
                 database="roommate_app"
             )
 
@@ -139,8 +169,8 @@ def get_student_id_by_ac_fan_preference(gender, ac_fan_preference):
 
             db_connection = mysql.connector.connect(
                 host="localhost",
-                user="root",
-                password="your_local_mysql_password",
+                user="your_mysql_username",
+                password="your_mysql_password",
                 database="roommate_app"
             )
             cur = db_connection.cursor()
@@ -166,8 +196,8 @@ def get_student_id_by_ac_fan_preference(gender, ac_fan_preference):
 
             db_connection = mysql.connector.connect(
                 host="localhost",
-                user="root",
-                password="your_local_mysql_password",
+                user="your_mysql_username",
+                password="your_mysql_password",
                 database="roommate_app"
             )
 
@@ -191,8 +221,8 @@ def get_student_id_by_ac_fan_preference(gender, ac_fan_preference):
 
             db_connection = mysql.connector.connect(
                 host="localhost",
-                user="root",
-                password="your_local_mysql_password",
+                user="your_mysql_username",
+                password="your_mysql_password",
                 database="roommate_app"
             )
 
@@ -210,7 +240,7 @@ def get_student_id_by_ac_fan_preference(gender, ac_fan_preference):
 
 # function to search students who have both an ac and fan preference:
 
-# @app.route('/api/get_student_id_by_ac_preference_fan_preference/')
+@app.route('/get_student_id_by_ac_preference_fan_preference', methods=['GET', 'POST'])
 def get_student_id_by_ac_temp(gender, ac_temp_preference):
     """
     ac_temp = int
@@ -230,8 +260,8 @@ def get_student_id_by_ac_temp(gender, ac_temp_preference):
 
         db_connection = mysql.connector.connect(
             host="localhost",
-            user="root",
-            password="your_local_mysql_password",
+            user="your_mysql_username",
+            password="your_mysql_password",
             database="roommate_app"
         )
 
@@ -254,7 +284,7 @@ def get_student_id_by_ac_temp(gender, ac_temp_preference):
         cur.close()
 
 
-# @app.route('/api/select_roommate_preference/')
+@app.route('/select_roommate_preference', methods=['GET', 'POST'])
 def select_roommate_preference(student_id, roommate_id, room_number):
     """
     Calling this function would look like:
@@ -317,7 +347,10 @@ def select_roommate_preference(student_id, roommate_id, room_number):
 
 """
 
-#get_student_ids_by_gender('F')
-#get_student_id_by_bedtime_preference('M', 23)
+#get_student_ids_by_gender('M')
+#get_student_id_by_bedtime_preference('F', 23)
 #get_student_id_by_ac_fan_preference('F', ('ac', ''))
 #get_student_id_by_ac_temp('M', 23)
+
+if __name__ == "__main__":
+    app.run(debug=True)
